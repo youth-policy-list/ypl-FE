e891d7c4c46b13d9a8fad9cfb2587be274f21c15
aaron0331 <luminox93@naver.com>
2022-08-03 07:49:08 +0900

    components 폴더 설정

---
+++ src/components/banner/SlideBanner.js
@@ -0,0 +1,17 @@
+import "./SlideBanner.css";
+
+const SlideBanner = () => {
+  return (
+    <article className="banner-container">
+      <div className="title-container">
+        <h3>Title name</h3>
+        <p>Category</p>
+      </div>
+      <div className="sub-container">
+        <p>sub</p>
+      </div>
+    </article>
+  );
+};
+
+export default SlideBanner;
