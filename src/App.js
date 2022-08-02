5dab4c51dfd686f547dc92543a00be17abde914b
aaron0331 <luminox93@naver.com>
2022-08-03 08:50:37 +0900

    PolicyCategory 작성 후 merge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import SlideBanner from "./components/banner/SlideBanner";
+import PolicyCategory from "./components/policies/PolicyCategory";
 
 function App() {
   return (
     <div className="App">
       <SlideBanner />
+      <PolicyCategory />
     </div>
   );
 }
