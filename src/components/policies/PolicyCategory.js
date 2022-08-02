14f73b2be017a269e7c917d2deb41a4a70e515f0
aaron0331 <luminox93@naver.com>
2022-08-03 08:49:13 +0900

    PolicyCategory 컴포넌트 작성

--- src/components/policies/PolicyCategory.js
+++ src/components/policies/PolicyCategory.js
@@ -1,5 +1,19 @@
+import "./PolicyCategory.css";
+
 const PolicyCategory = () => {
-  return <div>test</div>;
+  return (
+    <article className="category__container">
+      <div className="category__img-container">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3135/3135726.png"
+          alt="test"
+        ></img>
+      </div>
+      <div className="category__title-container">
+        <p>취업지원</p>
+      </div>
+    </article>
+  );
 };
 
 export default PolicyCategory;
