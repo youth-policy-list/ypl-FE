8fca56876ad034038c81bd04dfdb8c9a599b94bf
aaron0331 <luminox93@naver.com>
2022-08-03 08:48:43 +0900

    className 수정

--- src/components/banner/SlideBanner.js
+++ src/components/banner/SlideBanner.js
@@ -2,12 +2,12 @@ import "./SlideBanner.css";
 
 const SlideBanner = () => {
   return (
-    <article className="banner-container">
-      <div className="title-container">
+    <article className="banner__container">
+      <div className="banner__title-container">
         <h3>Title name</h3>
         <p>Category</p>
       </div>
-      <div className="sub-container">
+      <div className="banner__sub-container">
         <p>sub</p>
       </div>
     </article>
